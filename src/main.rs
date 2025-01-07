5d36dc55f0d4e0da1192c5c45c8bed16aac348c3
anonymousUsernameForPaper <124139444+anonymousUsernameForPaper@users.noreply.github.com>
2025-01-07 17:14:20 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-// examples todo:
-// - bool, f32
-// - crossover example
-// - with reorder, dag, etc.
-// standard bool with json
-
 #![allow(dead_code)]
 #![allow(unused_mut)]
 #![allow(unused_imports)]
@@ -34,7 +28,6 @@ use cgp_master::components::evo_operators_for_population::mutation_operators::mu
 use cgp_master::components::evo_operators_for_population::selection_operators::elitist_selection_oneplusfour::PopulationElitistSelectionOnePlusFour;
 use cgp_master::components::evo_operators_for_population::mutation_operators::mutation_trait::PopulationGeneralMutation;
 use cgp_master::components::evo_operators_for_population::selection_operators::selection_trait::PopulationGeneralSelection;
-// use cgp_master::components::evo_operators_for_population::mutation_operators::mutate_population_multi_single::EAMutateDecreasingMultiN;
 
 use cgp_master::utils::runner::ProgramState;
 use cgp_master::utils::configurator::*;
@@ -46,7 +39,7 @@ use cgp_master::utils::logger_functions::{LoggerActiveNodes, LoggerFitness};
 use cgp_master::utils::checkpoint::*;
 
 
-fn crossover_example(args: Cli) {
+fn standard_bool(args: Cli) {
     let (data, label) = match args.dataset_args.dataset.as_str() {
         "Parity" => boolean_datasets::parity::get_dataset(),
         "Encode" => boolean_datasets::encode::get_dataset(),
@@ -54,79 +47,6 @@ fn crossover_example(args: Cli) {
         "Multiply" => boolean_datasets::multiply::get_dataset(),
         _ => { panic!("Unknown dataset type {}", args.dataset_args.dataset_type) }
     };
-
-    let function_set = boolean_function_set::get_boolean_function_set();
-
-    let params = make_cgp_params(&args, data[0].len(), label[0].len(), function_set.len());
-
-    let node_mutation_op = Rc::new(get_node_mutation_operator(&params));
-    let chromosome_active_op = Rc::new(get_active_node_finder_operator(&params));
-
-    let chromosome_mutation_op: Rc<Box<dyn ChromosomeMutation>> = Rc::new(get_chromosome_mutation_operator(&params));
-
-    let chromosome_eval_op = Rc::new(ChromosomeEvaluatorGeneral::new());
-
-    // let clone_parent2child = CloneParentToChild::new();
-    let mut mutation_operator = PopulationMutationGeneral::new();
-    let eval_operator = get_population_evaluator_operator(&params);
-    let selection_operator = get_population_selection_operator(&params);
-
-    let mut runner = ProgramState::new(params.clone(), data, label, None, None, Rc::clone(&function_set), Rc::clone(&chromosome_eval_op), Rc::clone(&chromosome_active_op));
-
-
-    let mut logger_fitness = LoggerFitness::new(&args, &params);
-    let mut logger_active_nodes = LoggerActiveNodes::new(&args, &params);
-
-    let (crossover_population_mechanism, crossover_operator) = get_population_crossover_operator(&params);
-
-    let save_path = Path::new("")
-        .join(format!("Experiments_Output_{}", args.dataset_args.dataset_type))
-        .join(format!("cgp_extension_type_{}", args.cgp_extension_type))
-        .join(format!("dataset_{}", args.dataset_args.dataset))
-        .join(format!("mutation_type_{}", args.mutation_args.mutation_type))
-        .join(format!("selection_type_{}", args.selection_args.selection_type))
-        .join(format!("crossover_type_{}", args.crossover_args.crossover_type))
-        .join(format!("run_id_{}", args.run_id));
-    let save_file_iteration = format!("mutated_nodes_{}.txt", args.run_id);
-    let mut mutationfile = File::create(save_path.join(save_file_iteration))
-        .expect("cannot create file");
-
-    let mut iteration_number = 0;
-
-    selection_operator.execute(&mut runner);
-    for i in 0..500_000 {
-        logger_fitness.write_fitness(iteration_number, runner.get_best_fitness());
-        if i % 500 == 0 {
-            println!("i: {}, fitness: {}", i, runner.get_best_fitness());
-        }
-        iteration_number += 1;
-
-        // clone_parent2child.execute(&mut runner);
-        crossover_population_mechanism.execute(&mut runner, Rc::clone(&chromosome_active_op), Rc::clone(&function_set), Rc::clone(&crossover_operator));
-        mutation_operator.execute(&mut runner, Rc::clone(&node_mutation_op), Rc::clone(&chromosome_mutation_op));
-        eval_operator.execute(&mut runner, Rc::clone(&chromosome_eval_op), Rc::clone(&chromosome_active_op), Rc::clone(&function_set));
-        selection_operator.execute(&mut runner);
-
-        if runner.get_best_fitness() < params.fitness_threshold {
-            break;
-        }
-    }
-    println!("{}", iteration_number);
-
-    logger_fitness.write_finished_fitness(iteration_number, None, None);
-
-    logger_active_nodes.write_active_nodes(&mut runner, Rc::clone(&chromosome_active_op), Rc::clone(&function_set));
-}
-
-fn bioma_bool(args: Cli) {
-    let (data, label) = match args.dataset_args.dataset.as_str() {
-        "Parity" => boolean_datasets::parity::get_dataset(),
-        "Encode" => boolean_datasets::encode::get_dataset(),
-        "Decode" => boolean_datasets::decode::get_dataset(),
-        "Multiply" => boolean_datasets::multiply::get_dataset(),
-        _ => { panic!("Unknown dataset type {}", args.dataset_args.dataset_type) }
-    };
-
 
     let function_set = boolean_function_set::get_boolean_function_set();
 
@@ -184,169 +104,11 @@ fn bioma_bool(args: Cli) {
     println!("{}", iteration_number);
 
     logger_fitness.write_finished_fitness(iteration_number, None, None);
-
-    logger_active_nodes.write_active_nodes(&mut runner, Rc::clone(&chromosome_active_op), Rc::clone(&function_set));
-}
-
-fn bioma_f32(args: Cli) {
-    let (train_data, train_label) = match args.dataset_args.dataset.as_str() {
-        "Keijzer6" => regression_benchmarks::keijzer::get_dataset(),
-        "Koza3" => regression_benchmarks::koza_3::get_dataset(),
-        "Nguyen7" => regression_benchmarks::nguyen_7::get_dataset(),
-        "Pagie1" => regression_benchmarks::pagie_1::get_dataset(),
-        _ => { panic!("Unknown dataset type {}", args.dataset_args.dataset_type) }
-    };
-    let (eval_data, eval_label) = match args.dataset_args.dataset.as_str() {
-        "Keijzer6" => regression_benchmarks::keijzer::get_eval_dataset(),
-        "Koza3" => regression_benchmarks::koza_3::get_eval_dataset(),
-        "Nguyen7" => regression_benchmarks::nguyen_7::get_eval_dataset(),
-        "Pagie1" => regression_benchmarks::pagie_1::get_eval_dataset(),
-        _ => { panic!("Unknown dataset type {}", args.dataset_args.dataset_type) }
-    };
-
-    let function_set = regression_function_set::get_regression_function_set();
-
-    let params = make_cgp_params(&args, train_data[0].len(), train_label[0].len(), function_set.len());
-
-    let node_mutation_op = Rc::new(get_node_mutation_operator(&params));
-    let chromosome_active_op = Rc::new(get_active_node_finder_operator(&params));
-
-    let chromosome_mutation_op: Rc<Box<dyn ChromosomeMutation>> = Rc::new(get_chromosome_mutation_operator(&params));
-
-    let chromosome_eval_op = Rc::new(ChromosomeEvaluatorGeneral::new());
-
-    let clone_parent2child = CloneParentToChild::new();
-    let mut mutation_operator = PopulationMutationGeneral::new();
-    let eval_operator = get_population_evaluator_operator(&params);
-    let selection_operator = get_population_selection_operator(&params);
-
-    let mut runner = ProgramState::new(params.clone(), train_data, train_label, Some(eval_data), Some(eval_label), Rc::clone(&function_set), Rc::clone(&chromosome_eval_op), Rc::clone(&chromosome_active_op));
-
-
-    let mut logger_fitness = LoggerFitness::new(&args, &params);
-    let mut logger_active_nodes = LoggerActiveNodes::new(&args, &params);
-
-    let save_path = Path::new("")
-        .join(format!("Experiments_Output_{}", args.dataset_args.dataset_type))
-        .join(format!("cgp_extension_type_{}", args.cgp_extension_type))
-        .join(format!("dataset_{}", args.dataset_args.dataset))
-        .join(format!("mutation_type_{}", args.mutation_args.mutation_type))
-        .join(format!("selection_type_{}", args.selection_args.selection_type))
-        .join(format!("crossover_type_{}", args.crossover_args.crossover_type))
-        .join(format!("run_id_{}", args.run_id));
-    let save_file_iteration = format!("mutated_nodes_{}.txt", args.run_id);
-    let mut mutationfile = File::create(save_path.join(save_file_iteration))
-        .expect("cannot create file");
-
-
-    let mut iteration_number = 0;
-    for i in 0..500_000 {
-        logger_fitness.write_fitness(iteration_number, runner.get_best_fitness());
-
-        // writeln!(output_file, "Iteration: {iteration_number}, Fitness: {:?}", runner.get_best_fitness()).expect("write not okay??");
-        if i % 500 == 0 {
-            println!("i: {}, fitness: {}", iteration_number, runner.get_best_fitness());
-        }
-
-        iteration_number += 1;
-
-        clone_parent2child.execute(&mut runner);
-        mutation_operator.execute(&mut runner, Rc::clone(&node_mutation_op), Rc::clone(&chromosome_mutation_op));
-        eval_operator.execute(&mut runner, Rc::clone(&chromosome_eval_op), Rc::clone(&chromosome_active_op), Rc::clone(&function_set));
-        selection_operator.execute(&mut runner);
-
-        if runner.get_best_fitness() < params.fitness_threshold {
-            break;
-        }
-    }
-
-    let fitness_testset = eval_operator.get_test_fitness(&mut runner, Rc::clone(&chromosome_eval_op), Rc::clone(&chromosome_active_op), Rc::clone(&function_set));
-
-    logger_fitness.write_finished_fitness(iteration_number, Some(runner.get_best_fitness()), Some(fitness_testset));
-
-    logger_active_nodes.write_active_nodes(&mut runner, Rc::clone(&chromosome_active_op), Rc::clone(&function_set));
-}
-
-fn bioma_uci(args: Cli) {
-    let (train_data, train_label, eval_data, eval_label) = match args.dataset_args.dataset.as_str() {
-        "abalone" => real_world_uci::abalone::get_dataset(r"C:\Users\Henning\IdeaProjects\cgp_master\src\datasets\real_world_uci\data\abalone.data".to_string(), false),
-        "cancer" => real_world_uci::breast_cancer::get_dataset(r"C:\Users\Henning\IdeaProjects\cgp_master\src\datasets\real_world_uci\data\breast+cancer+wisconsin+diagnostic.data".to_string(), false),
-        "page" => real_world_uci::page_blocks::get_dataset(r"C:\Users\Henning\IdeaProjects\cgp_master\src\datasets\real_world_uci\data\page-blocks.data".to_string(), false),
-        _ => { panic!("Unknown dataset type {}", args.dataset_args.dataset_type) }
-    };
-
-    let function_set = regression_function_set::get_regression_function_set();
-
-    let params = make_cgp_params(&args, train_data[0].len(), train_label[0].len(), function_set.len());
-
-    let node_mutation_op = Rc::new(get_node_mutation_operator(&params));
-    let chromosome_active_op = Rc::new(get_active_node_finder_operator(&params));
-
-    let chromosome_mutation_op: Rc<Box<dyn ChromosomeMutation>> = Rc::new(get_chromosome_mutation_operator(&params));
-
-    let chromosome_eval_op = Rc::new(ChromosomeEvaluatorGeneral::new());
-
-    let clone_parent2child = CloneParentToChild::new();
-    let mut mutation_operator = PopulationMutationGeneral::new();
-    let eval_operator = get_population_evaluator_operator(&params);
-    let selection_operator = get_population_selection_operator(&params);
-
-    let mut runner = ProgramState::new(params.clone(), train_data, train_label, Some(eval_data), Some(eval_label), Rc::clone(&function_set), Rc::clone(&chromosome_eval_op), Rc::clone(&chromosome_active_op));
-
-
-    let mut logger_fitness = LoggerFitness::new(&args, &params);
-    let mut logger_active_nodes = LoggerActiveNodes::new(&args, &params);
-
-    let save_path = Path::new("")
-        .join(format!("Experiments_Output_{}", args.dataset_args.dataset_type))
-        .join(format!("cgp_extension_type_{}", args.cgp_extension_type))
-        .join(format!("dataset_{}", args.dataset_args.dataset))
-        .join(format!("mutation_type_{}", args.mutation_args.mutation_type))
-        .join(format!("selection_type_{}", args.selection_args.selection_type))
-        .join(format!("crossover_type_{}", args.crossover_args.crossover_type))
-        .join(format!("run_id_{}", args.run_id));
-    let save_file_iteration = format!("mutated_nodes_{}.txt", args.run_id);
-    let mut mutationfile = File::create(save_path.join(save_file_iteration))
-        .expect("cannot create file");
-
-
-    let mut iteration_number = 0;
-    for i in 0..100_000 {
-        logger_fitness.write_fitness(iteration_number, runner.get_best_fitness());
-
-        // writeln!(output_file, "Iteration: {iteration_number}, Fitness: {:?}", runner.get_best_fitness()).expect("write not okay??");
-        if i % 500 == 0 {
-            println!("i: {}, fitness: {}", iteration_number, runner.get_best_fitness());
-        }
-
-        iteration_number += 1;
-
-        clone_parent2child.execute(&mut runner);
-        mutation_operator.execute(&mut runner, Rc::clone(&node_mutation_op), Rc::clone(&chromosome_mutation_op));
-        eval_operator.execute(&mut runner, Rc::clone(&chromosome_eval_op), Rc::clone(&chromosome_active_op), Rc::clone(&function_set));
-        selection_operator.execute(&mut runner);
-
-        if runner.get_best_fitness() < params.fitness_threshold {
-            break;
-        }
-    }
-
-    let fitness_testset = eval_operator.get_test_fitness(&mut runner, Rc::clone(&chromosome_eval_op), Rc::clone(&chromosome_active_op), Rc::clone(&function_set));
-
-    logger_fitness.write_finished_fitness(iteration_number, Some(runner.get_best_fitness()), Some(fitness_testset));
 
     logger_active_nodes.write_active_nodes(&mut runner, Rc::clone(&chromosome_active_op), Rc::clone(&function_set));
 }
 
 fn main() {
     let args = get_arguments();
-
-    // bioma_bool(args);
-    // bioma_f32(args);
-    bioma_uci(args);
-    // if args.dataset_args.dataset_type == "bool" {
-    //     bioma_bool(args);
-    // } else {
-    //     bioma_f32(args);
-    // }
+    standard_bool(args);
 }
